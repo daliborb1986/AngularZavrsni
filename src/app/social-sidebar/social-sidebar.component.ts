ea3e273f63ec4408947a04cda25e480105b43a36
daliborb1986 <118627855+daliborb1986@users.noreply.github.com>
2024-06-29 19:05:39 +0200

    done with social icons

--- src/app/social-sidebar/social-sidebar.component.ts
+++ src/app/social-sidebar/social-sidebar.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-social-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './social-sidebar.component.html',
   styleUrl: './social-sidebar.component.css',
 })
-export class SocialSidebarComponent {}
+export class SocialSidebarComponent implements OnInit {
+  socialIcons: any[] = [
+    {
+      name: 'facebook',
+      path: '/assets/facebook.png',
+    },
+    {
+      name: 'twitter',
+      path: '/assets/twitter.png',
+    },
+    {
+      name: 'pinterest',
+      path: '/assets/pinterest.png',
+    },
+    {
+      name: 'youtube',
+      path: '/assets/youtube.png',
+    },
+    {
+      name: 'instagram',
+      path: '/assets/instagram.png',
+    },
+  ];
+  ngOnInit(): void {}
+}
