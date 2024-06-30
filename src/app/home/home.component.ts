a07368922634312d5f55e96451ac7d1c0bf5b6bb
daliborb1986 <118627855+daliborb1986@users.noreply.github.com>
2024-06-30 13:27:09 +0200

    fixed everything

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
+
 @Component({
   selector: 'app-home',
   standalone: true,
