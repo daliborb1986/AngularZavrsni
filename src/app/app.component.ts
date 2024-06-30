2f596df278be1a5957cccd84d60db6d7e3f866ed
daliborb1986 <118627855+daliborb1986@users.noreply.github.com>
2024-06-30 11:24:33 +0200

    fixed navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { SocialSidebarComponent } from "./social-sidebar/social-sidebar.component";
 import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from "./footer/footer.component";
 
 
 @Component({
@@ -10,7 +11,7 @@ import { HomeComponent } from "./home/home.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, SocialSidebarComponent, HomeComponent]
+    imports: [RouterOutlet, HeaderComponent, SocialSidebarComponent, HomeComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'AngularZavrsni';
