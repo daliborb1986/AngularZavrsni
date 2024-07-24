691ca070cad3a8bfa6f8af1b3ace0f754fa057dc
daliborb1986 <118627855+daliborb1986@users.noreply.github.com>
2024-07-24 17:31:57 +0200

    signup form done, validation to do

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { CategoryComponent } from './category/category.component';
 import { ProductComponent } from './product/product.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { LoginComponent } from './login/login.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   {
@@ -19,6 +20,7 @@ export const routes: Routes = [
   },
   { path: 'shop/:category/:title', component: ProductComponent },
   {path: 'shoppingCart', component: ShoppingCartComponent},
+  {path:'login', component: LoginComponent},
   { path: 'not-found', component: PageNotFoundComponent },
   { path: '**', redirectTo: '/not-found' },
   
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  @ViewChild('myForm') signupForm: NgForm;
+onSubmit() {
+  console.log(this.signupForm)
+}
 }
